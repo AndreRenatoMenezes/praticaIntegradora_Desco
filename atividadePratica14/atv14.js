50c48c766f9c971940ae3aae905210e73bdbc020
AndreRenatoMenezes <andrenato.ti@gmail.com>
2023-05-28 13:49:42 -0300

    Finalizado atv 14

--- atividadePratica14/atv14.js
+++ atividadePratica14/atv14.js
@@ -1,24 +1,16 @@
-// Criando uma matriz com 4 linhas e 5 colunas
+//Atividade Prática 14 - Arrays II
+//Aluno: André Renato Silva Braga de Menezes
 var matriz = new Array(4);
 for (var i = 0; i < matriz.length; i++) {
     matriz[i] = new Array(5);
 }
-
-// Preenchendo a matriz com os múltiplos
-for (var linha = 0; linha < matriz.length; linha++) {
-    for (var coluna = 0; coluna < matriz[linha].length; coluna++) {
-        // Determinando o número base para cada linha
-        var numeroBase = linha + 2;
-
-        // Calculando o múltiplo atual
-        var multiplo = numeroBase * (coluna + 1);
-
-        // Preenchendo o valor na matriz
-        matriz[linha][coluna] = multiplo;
+for (var lin = 0; lin < matriz.length; lin++) {
+    for (var col = 0; col < matriz[lin].length; col++) {
+        var numeroBase = lin + 2;
+        var mult = numeroBase * (col + 1);
+        matriz[lin][col] = mult;
     }
 }
-
-// Exibindo a matriz no console
 for (var i = 0; i < matriz.length; i++) {
     console.log(matriz[i]);
 }
